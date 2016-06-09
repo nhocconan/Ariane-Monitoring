8d3046b597568c9810b2289a38457ace7e8c8f2a
Ne00n <Ne00n@users.noreply.github.com>
2016-06-09 15:17:32 +0200

    Update 14.4.11
    - Added MySQL Connection check to Tests

--- pages/tests.php
+++ pages/tests.php
@@ -4,9 +4,13 @@ include 'functions.php';
 
 class Test extends PHPUnit_Framework_TestCase
 {
+	public function testMySQLConnection() {
+	  $this->assertEquals($mysqli->connect_error,NULL);
+	}
+
 	public function testEscape() {
-    $result = escape("<script>alert('attacked')</script>");
-		$this->assertEquals($result,"&lt;script&gt;alert(&#039;attacked&#039;)&lt;/script&gt;");
+  	  $result = escape("<script>alert('attacked')</script>");
+	  $this->assertEquals($result,"&lt;script&gt;alert(&#039;attacked&#039;)&lt;/script&gt;");
   	}
 }
 
