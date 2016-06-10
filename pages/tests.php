d81a8a18dfba82d063d778d19c4ead16e1fcd9e4
Ne00n <neo@tiefkuehler-madness.me>
2016-06-10 18:06:29 +0200

    Update 14.4.15
    - Removed Connection Check

--- pages/tests.php
+++ pages/tests.php
@@ -4,23 +4,16 @@ include 'functions.php';
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function testMySQLConnection() {
-	  global $mysqli;
-	  $this->assertEquals($mysqli->connect_error,NULL);
-	}
-
 	public function testEscape() {
     $result = escape("<script>alert('attacked')</script>");
 	  $this->assertEquals($result,"&lt;script&gt;alert(&#039;attacked&#039;)&lt;/script&gt;");
   	}
 
 	public function testCheckAccess_True() {
-		global $mysqli;
 		$this->assertEquals(checkAccess(1,1),1);
 	}
 
 	public function testCheckAccess_False() {
-		global $mysqli;
 		$this->assertEquals(checkAccess(5,1),0);
 	}
 }
