273d9023093951d20e99d7416e9a8b6c89951fd1
Ne00n <neo@tiefkuehler-madness.me>
2016-06-18 19:47:41 +0200

    Update 14.5.5
    - Added Constant for the Domain

--- pages/config.php
+++ pages/config.php
@@ -5,6 +5,9 @@ $mysqli = new mysqli("localhost", "root", "", "ariane");
 //Timezone
 date_default_timezone_set('Europe/Amsterdam');
 
+//URL, Rootfolder of Ariane
+define("_URL","http://yourpage.com/ariane/");
+
 //Email alerts
 define("_email_sender","noreply@yoursite.net");
 define("_email_target","alert@yoursite.net");
