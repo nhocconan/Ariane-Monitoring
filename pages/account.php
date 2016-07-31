3263ae018b0eede16dbd046577fc72d77f6a1580
Ne00n <neo@tiefkuehler-madness.me>
2016-07-31 23:22:07 +0200

    Moved Login/Account to Pages

---
+++ pages/account.php
@@ -0,0 +1,102 @@
+<?php
+
+$msg = "";
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+if (isset($_POST['confirm'])) {
+
+ $old_pw = $_POST['old_pw'];
+ $pw = $_POST['new_pw'];
+ $pw2 = $_POST['new_pw2'];
+
+ $success = true;
+
+ if (strlen($pw) < 10 ) {$msg = "Passwords to short."; $success = false;}
+ if (strlen($pw) > 160 ) {$msg = "Passwords are to long."; $success = false;}
+ if ($pw != $pw2) {$msg = "Passwords not equal."; $success = false;}
+
+ $stmt = $mysqli->prepare("SELECT password FROM users WHERE id = ?");
+ $stmt->bind_param('i', $USER_ID);
+ $stmt->execute();
+ $stmt->bind_result($password_db);
+ $stmt->fetch();
+ $stmt->close();
+
+ if ($password_db == NULL) {
+   session_unset();
+   session_destroy();
+   header('Location: index.php');
+ }
+
+ if ($success == true) {
+   if (password_verify($old_pw, $password_db)) {
+
+     $hash = password_hash($pw, PASSWORD_DEFAULT);
+
+     $stmt = $mysqli->prepare("UPDATE users SET password = ?  WHERE id = ?");
+     $stmt->bind_param('si',$hash,$_SESSION['user_id']);
+     $stmt->execute();
+     $stmt->close();
+
+     $msg = "Password changed.";
+     $success = true;
+   } else {
+     $msg = "Old Password is incorrect.";
+     $success = false;
+   }
+ }
+}
+}
+
+
+?>
+
+<div class="col-md-4 col-md-offset-4" style="background-color:white;opacity:0.85;margin-top:150px;border-radius:8px;">
+  <ul class="nav nav-tabs">
+    <li><a href="index.php?page=dashboard">Servers</a></li>
+    <li><a href="index.php?page=dashboard?add">Add Server</a></li>
+    <li class="active"><a href="index.php?page=dashboard?account">Account</a></li>
+    <li><a href="index.php?page=logout">Logout</a></li>
+  </ul>
+  <form class="form-horizontal"  action="index.php?page=dashboard?account" method="post" >
+    <h3>Account Password</h3>
+    <?php
+      if ($msg != "" and $success == false) {
+        echo'<div class="alert alert-danger col-md-12" style="text-align: center;">
+              <h2>Error!</h3>
+              <p>'.$msg.'</p>
+              </div>';
+      } elseif ($msg != "" and $success == true) {
+        echo'<div class="alert alert-success col-md-12" style="text-align: center;">
+              <h2>Okay!</h3>
+              <p>'.$msg.'</p>
+              </div>';
+      }
+     ?>
+   <div class="form-group">
+       <label for="inputEmail" class="control-label col-xs-2">Old:</label>
+       <div class="col-xs-6">
+           <input type="password" class="form-control" placeholder="Old Password" name="old_pw">
+       </div>
+   </div>
+   <div class="form-group">
+       <label for="inputPassword" class="control-label col-xs-2">New:</label>
+       <div class="col-xs-6">
+           <input type="password" class="form-control" placeholder="New Password" name="new_pw">
+       </div>
+   </div>
+   <div class="form-group">
+       <label for="inputPassword" class="control-label col-xs-2">Repeat:</label>
+       <div class="col-xs-6">
+           <input type="password" class="form-control" placeholder="Repeat Password" name="new_pw2">
+       </div>
+   </div>
+   <div class="form-group">
+       <div class="col-xs-offset-2 col-xs-10">
+           <button type="submit" name="confirm" class="btn btn-primary">Save</button>
+       </div>
+   </div>
+</form>
+
+  </div>
---
+++ pages/login.php
@@ -0,0 +1,77 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST' AND isset($_POST['confirm'])) {
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if(!preg_match("/^[a-zA-Z0-9]+$/",$username)){ $msg = "This Username contains invalid letters (a-z,A-Z,0-9 are allowed).<br>";  $error = true;}
+  if (strlen($password) < 10 ) {$msg = "The Password to short."; $error = true;}
+  if (strlen($password) > 160 ) {$msg = "The Password is to long."; $error = true;}
+  if (strlen($username) < 3 ) {$msg = "The Username is to short."; $error = true;}
+  if (strlen($username) > 50 ) {$msg = "The Username is to long."; $error = true;}
+  if ($password == "") { $msg = "You need to enter a Password"; $error = true;}
+  if ($username == "") { $msg = "You need to enter a Username"; $error = true;}
+
+    if ($error == false) {
+
+      $success = false;
+
+      $stmt = $mysqli->prepare("SELECT password,id FROM users WHERE username = ? LIMIT 1");
+      $stmt->bind_param('s', $username);
+      if ($stmt->execute()) { $success = true; }
+      $stmt->bind_result($password_db,$id);
+      $stmt->fetch();
+      $stmt->close();
+
+      if ($success) {
+        if (password_verify($password, $password_db)) {
+        // Success!
+        $_SESSION['login'] = 1;
+        $_SESSION['user_id'] = $id;
+        header('Location: index.php?page=dashboard');
+        } else {
+          $error = true;
+          $msg = "Incorrect Login.";
+        }
+      } else {
+        $error = true;
+        $msg = "MySQL Error.";
+      }
+    }
+}
+?>
+
+  <div class="container">
+      <div class="row vertical-offset-100">
+        <div class="col-md-4 col-md-offset-4">
+          <div class="panel panel-default box-opacity">
+            <div class="panel-heading">
+              <h3 class="panel-title">Please sign in</h3>
+            </div>
+            <div class="panel-body">
+            <?php if ($error == true) {
+              echo'<div class="alert alert-danger">
+                  <h4><center>Error!</center></h4>
+                  <p><center>'.$msg.'</center></p>
+                  </div>';  } ?>
+                <form action="index.php" method="post">
+                      <fieldset>
+                  <div class="form-group">
+                    <input class="form-control" placeholder="Username" name="username" type="text">
+                </div>
+                <div class="form-group">
+                  <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                </div>
+                <div class="checkbox">
+                    <!---<label>
+                      <input name="remember" type="checkbox" value="Remember Me"> Remember Me
+                    </label>-->
+                  </div>
+                <input class="btn btn-lg btn-success btn-block" type="submit" name="confirm" value="Login">
+              </fieldset>
+                </form>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
