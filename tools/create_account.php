0b81937fc0cc8759755041d1271924d2f7c5daad
Ne00n <neo@tiefkuehler-madness.me>
2016-08-12 18:41:11 +0200

    Create Account Fix

--- tools/create_account.php
+++ tools/create_account.php
@@ -1,13 +1,13 @@
 <?php
-exit();
-include 'functions.php';
+exit() //Remove this Line if you want to create an account, PUT IT BACK AFTER ADDING IT OR DELETE THE FILE
+include '../pages/functions.php';
 
 //Well change that stuff here, its just an example
 $name = "Test"; $password = "1234567899";
 
 $hash = password_hash($password, PASSWORD_DEFAULT);
 
-$stmt = $mysqli->prepare("INSERT INTO users(username,password) VALUES (?, ?)");
+$stmt = $database->prepare("INSERT INTO users(username,password) VALUES (?, ?)");
 $stmt->bind_param('ss', $name,$hash);
 $stmt->execute();
 $stmt->close();
